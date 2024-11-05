ba0f247ca152de1e646fa835e66c35981e1cc70a
royarchana <avniroy454@gmail.com>
2024-11-05 17:25:10 +0530

    file

---
+++ app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CardComponent from './CardComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <CardComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ cardComponent.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './main.css';
+
+function box(){
+    return(
+        <div className="box">
+            <div className="box-header">
+                <div className ="profilepic">1</div>
+                <div className = "title-section"> <h2 className="title">Title goes here</h2>
+                <p className="subtitle">Secondary Text</p>
+                </div>
+            </div>
+            //main image 
+            <div className="box-image">
+                <div className="image-placeholder">5</div>
+            </div>
+
+            
+            <p className="description">
+                Greyhound divisively hello coldly wonderfully marginally far upon excluding
+            </p>
+
+          
+            <div className="box-actions">
+                <ActionButton text="ACTION 1" />
+                <ActionButton text="ACTION 2" />
+                <div className="icon">❤</div>
+                <div className="icon">🔗</div>
+            </div>
+
+
+
+
+        </div>
+    );
+}
+export default box;
